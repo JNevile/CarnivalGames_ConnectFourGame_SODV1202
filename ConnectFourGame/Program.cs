a84b3312338be51abb0b818f10d385924ab6e815
JNevile <j.neville657@mybvc.ca>
2024-06-15 21:17:22 -0600

    Create player class, game class, start method and edit main method

--- ConnectFourGame/Program.cs
+++ ConnectFourGame/Program.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace ConnectFourGame
 {
-    internal class Program
+    //This class represents a player in the Connect Four Game
+    public class Player
+    {
+        public string Name { get; set; }
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+    //This class reperesents the Connect Four Game and it has two player Objects in this class
+    public class Game
+    {
+        private Player player1;
+        private Player player2;
+
+        public Game{string player1Name; string player2Name)
+        {
+            player1 = new Player(player1Name);
+            player2 - new Player(player2Name);
+        }
+        //This is the Start Method that contains the game logics
+        public void Start()
+        {
+            //To do insert game logic to allow placement of game pieces in game board 
+            //and have the game check for a winner (four in a row) after each turn
+            //Game logic checks for four matching color pieces (red or blue) in a row horizontally, vertically, or diagonlly.  
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
@@ -15,9 +43,13 @@ namespace ConnectFourGame
             //Ask each of the two players to enter their names to get started, starting with Player 1
             Console.WriteLine("Player 1, please enter your name. ");
             //Allow game player to answer by inputting text
-            string str = Console.ReadLine();
+            string player1Name = Console.ReadLine();
             Console.WriteLine("Player 2, please enter your name. ");
-            string str = Console.ReadLine();
+            string player2Name = Console.ReadLine();
+            //TO DO
+            //Add ability for game to respond to the players, displaying the name they inputted 
+            //and explain to player 1 they are red and to player 2 blue game pieces
+            //i.e. let them know player 1 is x and that represents red and player 2 is o and that represents blue - tbd
         }
     }
 }
